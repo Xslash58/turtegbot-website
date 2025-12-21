a4a77218c80c3f42e8b44cd9d6a8114360ecb7d6
Xslash <75801324+Xslash58@users.noreply.github.com>
2025-12-21 20:55:05 +0100

    add login route

---
+++ src/routes/login/twitch/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import LoadingIndicator from '../../../components/LoadingIndicator.svelte';
+
+	onMount(() => {
+		location.href = import.meta.env.VITE_AUTH_TWITCH_URL;
+	});
+</script>
+
+<section class="loading">
+	<p>Redirecting...</p>
+	<LoadingIndicator />
+</section>
+
+<style lang="scss">
+	section.loading {
+		flex-grow: 1;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.25rem;
+		font-weight: bold;
+
+		p {
+			text-align: center;
+			margin: 0 0 0.5rem 0;
+		}
+	}
+</style>
